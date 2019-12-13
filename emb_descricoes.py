34d84195f55d0ada82448c5eac55df815335be82
Daniel Prado <dani-dmello@hotmail.com>
2019-12-12 22:41:50 -0300

    rnn models

--- emb_descricoes.py
+++ emb_descricoes.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # In[24]:
-
 
 import shutil
 import errno
@@ -15,37 +13,51 @@ import codecs
 from keras.preprocessing.text import Tokenizer
 
 
-def preprocess():
+def preprocess(load = False):
     flag=0
     i = 0
-    count = 0
+    last_count = 0
+    new_count = 0
     lines = []
     PATH_PORTINARI = "../data/data-portinari/portinari_200x200/"
     
     portinari_desc = []
     retratos_ordem = []
-    retratos_desc = []
-
+    resto_ordem = []
+    
     with open(PATH_PORTINARI + "/tb_descricao_sem_crase.txt", encoding='latin-1') as openfile:
         for line in openfile:
-            portinari_desc.append(line)
-            i = i+1
+            line_without_number = line.split()[1:]
+            string_accumulator = ''
+            for word in line_without_number:
+                string_accumulator = string_accumulator + ' ' + word
+            portinari_desc.append(string_accumulator)            
+
             split = line.split()
             for part in split[-10:]:
                 if "Retrato" in part:
                     flag = 1
                 elif "de" in part and (flag==1):
-                    count = count + 1
+                    new_count = last_count + 1
                     flag = 0
-                    lines.append(line.split())
+                    line_without_number = line.split()[1:]
+                    lines.append(line)
+                    #string_accumulator = ''
+                    #for word in line_without_number:
+                    #    string_accumulator = string_accumulator + ' ' + word
                     retratos_ordem.append(i)
-                    retratos_desc.append(line)
+                    #retratos_desc.append(line_without_number)
                 else:
+                    #resto_ordem.append(i)
                     flag = 0
-    print('Achou {} retratos'.format(count))
-
-    retratos_idx_img = [int(l[0]) for l in lines]
+            if new_count == last_count:
+                resto_ordem.append(i)
+            last_count = new_count
+            i = i+1                
+    print('Achou {} retratos'.format(last_count))
+    
     '''
+    retratos_idx_img = [int(l[0]) for l in lines]
     for i in range(len(retratos_desc)):
         for j in range(len(retratos_desc[i])):
             if retratos_desc[i][j] == 'à':
@@ -55,73 +67,92 @@ def preprocess():
                 retratos_desc[i][j] = 'as'
                 print(retratos_desc[i][j])
             else:
-                continue'''
+                continue
+    '''
     
     MAX_NB_WORDS = 50000
+    #print(retratos_desc)
     tokenize = Tokenizer(nb_words = MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~ \t\n')
-    tokenize.fit_on_texts(retratos_desc)
-    retratos_idx = tokenize.texts_to_sequences(retratos_desc)
+    tokenize.fit_on_texts(portinari_desc)
+    portinari_idx = tokenize.texts_to_sequences(portinari_desc)
     word_index = tokenize.word_index
     index_word = {v: k for k, v in word_index.items()}
 
     key_list = list(word_index.keys())
     val_list = list(word_index.values())
 
-    first_tokens = [sq[0] for sq in retratos_idx]
-    first_words = [index_word[x[0]] for x in retratos_idx]
+    first_tokens = [sq[0] for sq in portinari_idx]
+    first_words = [index_word[x[0]] for x in portinari_idx]
 
     GLOVE_DIR = "../data/glove/"
     EMBEDDING_DIM = 300
     MAX_NB_WORDS = 50000000
+    
+    if(load):
+        try:
+            embedding_matrix = np.load('../data/data-portinari/embedding_matrix.npy')
+        except:
+            print('embedding not found!')
+    else:
+        print('Indexing word vectors.')
+        embeddings_index = {}
+        f = codecs.open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'), encoding='utf-8')
+        for line in f:
+            values = line.split(' ')
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+        f.close()
+        print('Found %s word vectors.' % len(embeddings_index))
 
-    '''print('Indexing word vectors.')
-    embeddings_index = {}
-    f = codecs.open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'), encoding='utf-8')
-    for line in f:
-        values = line.split(' ')
-        word = values[0]
-        coefs = np.asarray(values[1:], dtype='float32')
-        embeddings_index[word] = coefs
-    f.close()
-    print('Found %s word vectors.' % len(embeddings_index))
+        nb_words = min(MAX_NB_WORDS, len(word_index))
+        embedding_matrix = np.zeros((nb_words+2, EMBEDDING_DIM))
+        embedding_matrix[-1] = embeddings_index.get('<unk>')
+        unkown_tokens = []
+        for word, i in word_index.items():
+            if i >= nb_words:
+                continue
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+            else:
+                embedding_matrix[i] = embedding_matrix[nb_words+1]
+                #embedding_matrix[i] = embedding_matrix[0]
+                #unkown_tokens.append(i)
+        print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+        np.save('../data/data-portinari/embedding_matrix',embedding_matrix)
 
-    f = codecs.open(os.path.join(GLOVE_DIR, 'glove.6B.300d.txt'), encoding='utf-8')
-    emb_words = []
-    for line in f:
-        values = line.split(' ')
-        word = values[0]
-        emb_words.append(word)
-    f.close()
-
-    nb_words = min(MAX_NB_WORDS, len(word_index)) + 1
-
-    embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
-    for word, i in word_index.items():
-        if i >= nb_words:
-            continue
-        embedding_vector = embeddings_index.get(word)
-        if embedding_vector is not None:
-            # words not found in embedding index will be all-zeros.
-            embedding_matrix[i] = embedding_vector
-    print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
-    np.save('../data/data-celeba/embedding_matrix',embedding_matrix)'''
-    embedding_matrix = np.load('../data/data-celeba/embedding_matrix.npy')
-
-    zero_axis_emb = np.sum(embedding_matrix, axis=1) == 0
-    zero_axis = [i for i in range(len(zero_axis_emb)) if zero_axis_emb[i] == True]
+    emb_length = len(embedding_matrix)
+    #embedding_matrix = np.load('../data/data-celeba/embedding_matrix.npy')
+    index_word = {v: k for k, v in word_index.items()}
+    #index_word = {v: '<unk>' for k, v in word_index.items() if emb_length-1}
+    #index_word[nb_words+1] = '<unk>'
+    unknown_tokens = [i for i in range(len(embedding_matrix)) if np.sum(embedding_matrix[i]-embedding_matrix[-1]) == 0]
+    for tk in unknown_tokens:
+        index_word[tk] = '<unk>'
+    #zero_axis_emb = np.sum(embedding_matrix, axis=1) == 0
+    #zero_axis = [i for i in range(len(zero_axis_emb)) if zero_axis_emb[i] == True]
 
     null_words = []
     total_words = []
-    for i in retratos_idx:
+    for i in portinari_idx:
         for j in i:
             total_words.append(j)
-            if zero_axis_emb[j]:
+            if index_word[j] == '<unk>':
                 null_words.append(j)
-    retratos_len = [len(i) for i in retratos_desc]
+    #retratos_len = [len(i) for i in retratos_]
+    print(f'total words {len(total_words)}')
+    print(f'null words {len(null_words)}')
     print('{:.2f} % de palavras não encontradas no emb'.format(100*(len(null_words)/len(total_words))))
 
     zero_axis_emb = np.sum(embedding_matrix, axis=1) == 0
     zero_axis = [i for i in range(len(zero_axis_emb)) if zero_axis_emb[i] == True]
     
-    return embedding_matrix, retratos_idx, word_index, index_word
+    
+    #retratos_idx = [[word_index[word] for word in desc] for desc in retratos_desc]
+    retratos_idx = [portinari_idx[i] for i in retratos_ordem]
+    resto_idx = [portinari_idx[i] for i in resto_ordem]
+    
+    return embedding_matrix, portinari_idx, retratos_idx, resto_idx, word_index, index_word
 
--- text_models.py
+++ text_models.py
@@ -17,7 +17,209 @@ from text_models import *
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 
+import random
+from typing import Tuple
 
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class Encoder(nn.Module):
+    def __init__(self,
+                 pre_trained_embedding: float,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: float):
+        super().__init__()
+        
+        self.embedding = nn.Embedding.from_pretrained(
+            embeddings=pre_trained_embedding)
+        self.emb_dim = pre_trained_embedding.shape[1]
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.dropout = dropout
+            
+        self.rnn = nn.GRU(self.emb_dim, enc_hid_dim, bidirectional = True)
+
+        self.fc = nn.Linear(enc_hid_dim * 2, dec_hid_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self,
+                enc_input: Tensor, 
+                enc_input_len) -> Tuple[Tensor]:
+
+        embedded = self.dropout(self.embedding(enc_input))
+        
+        embedded_pack = torch.nn.utils.rnn.pack_padded_sequence(embedded, 
+            enc_input_len, enforce_sorted=False)
+        
+        #print(embedded_pack)
+        outputs_packed, hidden = self.rnn(embedded_pack)
+        #print(outputs_packed[0].shape)
+        #print(outputs_packed[1].shape)
+        #print(outputs_packed.shape)
+        outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs_packed)
+        #print(outputs.shape)
+        
+        hidden = torch.tanh(self.fc(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1)))
+        #print(hidden.shape)
+        
+        return outputs, hidden
+
+    
+class Attention(nn.Module):
+    def __init__(self,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 attn_dim: int):
+        super().__init__()
+
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+
+        self.attn_in = (enc_hid_dim * 2) + dec_hid_dim
+
+        self.attn = nn.Linear(self.attn_in, attn_dim)
+
+    def forward(self,
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor) -> Tensor:
+
+        src_len = encoder_outputs.shape[0]
+
+        repeated_decoder_hidden = decoder_hidden.unsqueeze(1).repeat(1, src_len, 1)
+
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+
+        energy = torch.tanh(self.attn(torch.cat((
+            repeated_decoder_hidden,
+            encoder_outputs),
+            dim = 2)))
+
+        attention = torch.sum(energy, dim=2)
+
+        return F.softmax(attention, dim=1)
+
+
+class Decoder(nn.Module):
+    def __init__(self,
+                 pre_trained_embedding: float,
+                 enc_hid_dim: int,
+                 dec_hid_dim: int,
+                 dropout: int,
+                 attention: nn.Module):
+        super().__init__()
+        
+        self.embedding = nn.Embedding.from_pretrained(
+            embeddings=pre_trained_embedding)
+        self.output_dim = pre_trained_embedding.shape[0]
+        self.emb_dim = pre_trained_embedding.shape[1]
+        self.enc_hid_dim = enc_hid_dim
+        self.dec_hid_dim = dec_hid_dim
+        self.dropout = dropout
+        self.attention = attention
+        
+        self.rnn = nn.GRU((enc_hid_dim * 2) + self.emb_dim, dec_hid_dim)
+
+        self.out = nn.Linear(self.attention.attn_in + self.emb_dim, self.output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+
+    def _weighted_encoder_rep(self,
+                              decoder_hidden: Tensor,
+                              encoder_outputs: Tensor) -> Tensor:
+
+        a = self.attention(decoder_hidden, encoder_outputs)
+
+        a = a.unsqueeze(1)
+        #print('a shape = {}'.format(a.shape))
+        
+        encoder_outputs = encoder_outputs.permute(1, 0, 2)
+        #print('encoder_outputs shape = {}'.format(encoder_outputs.shape))
+        
+        weighted_encoder_rep = torch.bmm(a, encoder_outputs)
+        #print('weighted_encoder_rep shape = {}'.format(weighted_encoder_rep.shape))
+        
+        weighted_encoder_rep = weighted_encoder_rep.permute(1, 0, 2)
+        #print('weighted_encoder_rep shape = {}'.format(weighted_encoder_rep.shape))
+        
+        return weighted_encoder_rep
+
+
+    def forward(self,
+                dec_input: Tensor,
+                #dec_input_len: Tensor, 
+                decoder_hidden: Tensor,
+                encoder_outputs: Tensor) -> Tuple[Tensor]:
+
+        dec_input = dec_input.unsqueeze(0)
+
+        embedded = self.dropout(self.embedding(dec_input))
+        #print(embedded.shape)
+        #print(f'embedded shape = {embedded.shape}')
+        weighted_h = self._weighted_encoder_rep(decoder_hidden,
+                                                encoder_outputs)
+        
+        #print(f'weighted_h shape = {weighted_h.shape}')
+        rnn_input = torch.cat((embedded, weighted_h), dim = 2)
+        
+        #print(rnn_input.shape)
+        #print(decoder_hidden.unsqueeze(0).shape)
+        output, decoder_hidden = self.rnn(rnn_input, decoder_hidden.unsqueeze(0))
+
+        embedded = embedded.squeeze(0)
+        output = output.squeeze(0)
+        weighted_h = weighted_h.squeeze(0)
+
+        output = self.out(torch.cat((output,
+                                     weighted_h,
+                                     embedded), dim = 1))
+
+        return output, decoder_hidden.squeeze(0)    
+
+class Seq2Seq(nn.Module):
+    def __init__(self,
+                 encoder: nn.Module,
+                 decoder: nn.Module,
+                 device: torch.device):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+
+    def forward(self,
+                x: Tensor,
+                x_l : Tensor,
+                teacher_forcing_ratio: float = 0.5) -> Tensor:
+
+        batch_size = x.shape[1]
+        max_len = x.shape[0]
+        vocab_size = self.decoder.output_dim
+
+        outputs = torch.zeros(max_len, batch_size, vocab_size).to(self.device)
+
+        encoder_outputs, hidden = self.encoder(x, x_l)
+
+        # first input to the decoder is the <sos> token
+        output = x[0,:]*0
+        #print(f'first output shape = {output.shape}')
+        
+        for t in range(0, max_len):
+            output, hidden = self.decoder(output, hidden, encoder_outputs)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.max(1)[1]
+            output = (x[t,:] if teacher_force else top1)
+            #if teacher_force: 
+            #    print(f'output teacher force = {output}')
+            
+        return outputs
+    
 class AE_LSTM(nn.Module):
     def __init__(self, nb_lstm_layers, nb_lstm_units=100, embedding_dim=300, nb_vocab_words = 3188, batch_size=32):
         super(AE_LSTM, self).__init__()
